381a72932b8fc5680bf6175c13cc7f323bb49314
Zubair Ahmed <zradtp@gmail.com>
2021-10-24 14:59:43 +0500

    JP_Assignment_Submit

---
+++ Assignments/Assignment #2/Assignment_2.py
@@ -0,0 +1,23 @@
+print ("Jawan Pakistan")
+print ("Assignment#2 by Zubair Ahmed (JP15119)")
+print ("============================")
+
+marks = 70
+
+if marks >= 90 and marks <=100:
+    print("You achieved Grade A+")
+elif marks >= 80 and marks <90:
+    print("You achieved Grade A")
+elif marks >= 70 and marks <80:
+    print("You achieved Grade B")
+elif marks >= 60 and marks <70:
+    print("You achieved Grade C")
+elif marks >= 50 and marks <60:
+    print("You achieved Grade D")
+elif marks >= 40 and marks <50:
+    print("You achieved Grade E")
+elif marks >= 0 and marks <40:
+    print("Try Again")
+    print("Rome Was Not Build In a Day")
+else:
+    print("Irrelevent marks")
